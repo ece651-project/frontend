5491d68c801fd37026b71678356eb886e0266739
San Lorenzo <j2534li@ca.uwaterloo>
2021-02-10 15:47:03 +0800

    2nd

---
+++ ctrl/picture.js
@@ -0,0 +1,31 @@
+(function () {
+    angular.module('app', ['ui.bootstrap', 'ngAnimate', 'httpservice'])
+        .controller('index_ctrl', IndexController)
+        .controller('ModalSignupCtrl', ModalSignupCtrl);
+
+    function IndexController($scope, $uibModal){
+        $scope.show_picture = function(a){
+            if(a==1)
+            $uibModal.open({
+                size:'md modal-dialog-centered',
+                templateUrl: 'signup1.html',
+                controller: 'ModalSignupCtrl',
+            }).result.catch(function(){});
+            else if(a==2)
+            $uibModal.open({
+                size:'md modal-dialog-centered',
+                templateUrl: 'signup2.html',
+                controller: 'ModalSignupCtrl',
+            }).result.catch(function(){});
+            else
+            $uibModal.open({
+                size:'md modal-dialog-centered',
+                templateUrl: 'signup3.html',
+                controller: 'ModalSignupCtrl',
+            }).result.catch(function(){});
+        }
+    }
+
+    function ModalSignupCtrl(){
+    }
+})();
