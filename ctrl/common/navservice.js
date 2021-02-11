edbdf973607737abe40772ff9179e86d6cb62d9e
shawnluxy <shawnluxy@gmail.com>
2021-02-11 04:39:28 -0500

    Create navservice

---
+++ ctrl/common/navservice.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('navservice', ['modalservice'])
+        .factory('NavHeaderService', NavHeaderService);
+
+    function NavHeaderService($rootScope, $window, ModalService){
+        var service = {};
+        service.navheader_init = function(back){
+            if(localStorage.hasOwnProperty("uid")){
+                $rootScope.navbutton1 = "Profile";
+                $rootScope.navbutton2 = "Log out";
+                $rootScope.to_login = function(){$window.location.href = "/page/profile.html";}
+                $rootScope.to_signup = function(){
+                    localStorage.removeItem("uid");
+                    back ? $window.location.href = "/index.html" : location.reload();
+                }
+            } 
+            else{
+                $rootScope.navbutton1 = "Log in";
+                $rootScope.navbutton2 = "Sign up";
+                $rootScope.to_login = ModalService.open_login;
+                $rootScope.to_signup = ModalService.open_signup;
+            }
+        }
+        return service;
+    }
+})();
--- ctrl/index.js
+++ ctrl/index.js
@@ -1,25 +1,11 @@
 (function () {
-    angular.module('app', ['modalservice'])
+    angular.module('app', ['navservice'])
         .controller('index_ctrl', IndexController);
     
-    function IndexController($scope, $window, ModalService){
-        if(localStorage.hasOwnProperty("uid")){
-            $scope.navbutton1 = "Profile";
-            $scope.navbutton2 = "Log out";
-            $scope.to_login = function(){$window.location.href = "/page/page.html";}
-            $scope.to_signup = function(){
-                localStorage.removeItem("uid");
-                location.reload();
-            }
-        } 
-        else{
-            $scope.navbutton1 = "Log in";
-            $scope.navbutton2 = "Sign up";
-            $scope.to_login = function(){ModalService.open_login();}
-            $scope.to_signup = function(){ModalService.open_signup();}
-        }
+    function IndexController($scope, $window, NavHeaderService){
+        NavHeaderService.navheader_init(true);
         $scope.to_aptlist = function(){
-            $window.location.href = "/page/page.html";
+            $window.location.href = "/page/profile.html";
         }
     }
 })();
