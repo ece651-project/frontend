ed43d4fb4ba51e574a2ee318911a1fde5b723dc9
ljn9888 <ljn9888@yeah.net>
2021-04-12 14:40:14 +0800

    merge 2 js controller to solve the picture tanchuang

--- ctrl/apt-info.js
+++ ctrl/apt-info.js
@@ -2,20 +2,59 @@
     angular.module('apt-info', ['ui.bootstrap', 'ngAnimate', 'ngSanitize', 'httpservice','navservice'])
         .controller('InfoController', InfoController)
         .controller('RatingDemoCtrl', RatingDemoCtrl)
-        .controller('CarouselDemoCtrl', CarouselDemoCtrl)
         .controller('siteCtrl', siteCtrl);
 
 
-    function siteCtrl($scope, $http, NavHeaderService) {
+    function siteCtrl($scope, $http, $uibModal, NavHeaderService) {
         $scope.Showcomment=true;
         NavHeaderService.navheader_init(false);
         $http.get("http://18.140.13.225:8080/user/get_apt/686ff1a5-dea1-4197-b091-3a177042e075").then(function (response) {
-            $scope.apt_info = response.data[0];
+            $scope.apt_info = response.xdata[0];
+            image_length=response.data[0].images.length;
         });
         $http.get("http://18.140.13.225:8080/user/get_user/686ff1a5-dea1-4197-b091-3a177042e075").then(function (response) {
             $scope.user_info = response.data;
         });
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        var currIndex = 0;
+        var picture_id = 0;
+        
+        $scope.addSlide = function() {
+          slides.push({
+            id: currIndex++
+          });
+        };
+        $scope.show_picture = function (picturenumber) {
+            if (picturenumber == 0)
+                $uibModal.open({
+                    size: 'md modal-dialog-centered',
+                    templateUrl: 'picture1.html',
+                }).result.catch(function () { });
+            else if (picturenumber == 1)
+                $uibModal.open({
+                    size: 'md modal-dialog-centered',
+                    templateUrl: 'picture2.html',
+                }).result.catch(function () { });
+                else if (picturenumber == 2)
+                $uibModal.open({
+                    size: 'md modal-dialog-centered',
+                    templateUrl: 'picture3.html',
+                }).result.catch(function () { });
+                else if (picturenumber == 3)
+                $uibModal.open({
+                    size: 'md modal-dialog-centered',
+                    templateUrl: 'picture4.html',
+                }).result.catch(function () { });}
+        for (var i = 0; i < 2; i++) {
+            picture_id =picture_id+1;
+            $scope.addSlide();
         }
+      
+        // Randomize logic below
+      }
+        
 
 
 
@@ -58,34 +97,7 @@
         }
     }
 
-    function CarouselDemoCtrl($scope,$uibModal){
-        $scope.myInterval = 5000;
-        $scope.noWrapSlides = false;
-        $scope.active = 0;
-        var slides = $scope.slides = [];
-        var currIndex = 0;
-        var picture_id = 4;
-        
-        $scope.addSlide = function() {
-          slides.push({
-            image1: 'iVBORw0KGgoAAAANSUhEUgAAABIAAAATCAIAAAAS8MqlAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyJpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw',
-            image: '../src/'+picture_id+'.jpg',
-            id: currIndex++
-          });
-        };
-        $scope.show_picture = function () {
-                $uibModal.open({
-                    size: 'md modal-dialog-centered',
-                    templateUrl: 'picture.html',
-                }).result.catch(function () { });}
-      
-        for (var i = 0; i < 2; i++) {
-            picture_id =picture_id+1;
-            $scope.addSlide();
-        }
-      
-        // Randomize logic below
-      }
+
 
 
 
